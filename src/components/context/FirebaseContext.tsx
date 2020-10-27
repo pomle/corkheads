eac578d15733ba1caa7a89b9745f736501228f87
Pontus Alexander <pontus.alexander@gmail.com>
2020-10-27 20:38:10 +0100

    Update Firebase config

--- src/components/context/FirebaseContext.tsx
+++ src/components/context/FirebaseContext.tsx
@@ -2,24 +2,14 @@ import React, { createContext, useContext } from "react";
 import * as firebase from 'firebase';
 import 'firebase/auth';
 import 'firebase/database';
+import config from "firebase.config.json";
 
 export const Context = createContext<firebase.app.App | null>(
   null
 );
 
 function createApp() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyDI8MJVGlBvld2kxMVwiiQt2rqEgxjB368",
-        authDomain: "corkheads-e9bcb.firebaseapp.com",
-        databaseURL: "https://corkheads-e9bcb.firebaseio.com",
-        projectId: "corkheads-e9bcb",
-        storageBucket: "corkheads-e9bcb.appspot.com",
-        messagingSenderId: "883288542166",
-        appId: "1:883288542166:web:132de2b1a2d6a12f75b277",
-        measurementId: "G-68X39M149Y"
-      };
-
-    return firebase.initializeApp(firebaseConfig);
+    return firebase.initializeApp(config);
 }
 
 const app = createApp();
