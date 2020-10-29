27d5906a2d55975cfd3c57b6531d03eb1d1282f6
Pontus Alexander <pontus.alexander@gmail.com>
2020-10-29 20:58:34 +0100

    Add ObjectStoreContext

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,9 +4,11 @@ import { SharedStateContext } from "components/context/SharedState";
 import { ContextMenuContext } from "components/context/ContextMenuContext";
 import { InternationalizationContext } from "components/context/InternationalizationContext";
 import { FirebaseContext } from "components/context/FirebaseContext";
+import { ObjectStoreContext } from "components/context/ObjectStoreContext";
 
 const contexts = [
   FirebaseContext,
+  ObjectStoreContext,
   ContextMenuContext,
   SharedStateContext,
   InternationalizationContext,
---
+++ app/src/components/context/ObjectStoreContext.tsx
@@ -0,0 +1,18 @@
+import React, { createContext, useContext, useState, Dispatch, SetStateAction } from "react";
+
+type Store = {
+    [key: string]: any
+}
+
+type ObjectStoreContextValue = [Store, Dispatch<SetStateAction<Store>>]
+
+const Context = createContext<ObjectStoreContextValue>([{}, () => undefined]);
+
+export const ObjectStoreContext: React.FC = ({ children }) => {
+  const state = useState<Store>(Object.create(null));
+  return <Context.Provider value={state}>{children}</Context.Provider>;
+};
+
+export function useObjectStore() {
+  return useContext(Context);
+}
