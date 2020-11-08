276d01df7abd2869de16b3747b8cc23b9101f97d
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-08 22:08:32 +0100

    Reenable explicit logout

--- app/src/components/hooks/useExplicitLogout.ts
+++ app/src/components/hooks/useExplicitLogout.ts
@@ -1,13 +1,13 @@
 import { useCallback } from "react";
 import { useHistory } from "react-router-dom";
-//import { useAuth } from "@internal/shared/hooks";
+import { useAuth } from "./useAuth";
 
 export const useExplicitLogout = () => {
   const history = useHistory();
-  //const auth = useAuth();
+  const { auth } = useAuth();
 
   return useCallback(async () => {
-    //await auth.signOut();
+    await auth.signOut();
     history.push("/");
-  }, [history]);
+  }, [auth, history]);
 };
