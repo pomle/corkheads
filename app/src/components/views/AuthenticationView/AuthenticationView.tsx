f6258826bff22a0a069aff26c7bd6bee18d0a8f9
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-08 22:09:04 +0100

    Use email link sign in

--- app/src/components/views/AuthenticationView/AuthenticationView.tsx
+++ app/src/components/views/AuthenticationView/AuthenticationView.tsx
@@ -1,25 +1,59 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import ViewStack from "components/ui/layout/ViewStack";
 import LoginView from "components/views/LoginView";
 import BusyView from "components/views/BusyView";
 import Fade from "components/ui/transitions/Fade";
 import { useHandler } from "components/hooks/useHandler";
 import { useAuth } from "components/hooks/useAuth";
+import { useDevicePreference } from "components/hooks/store/useDevicePreferences";
 
 const AuthenticationView: React.FC = () => {
   const { auth } = useAuth();
 
-  const submitHandle = useHandler(
-    useCallback(
-      (credentials: { username: string; password: string }) => {
-        return auth.signInWithEmailAndPassword(
-          credentials.username,
-          credentials.password
-        );
-      },
-      [auth]
-    )
+  const [email] = useDevicePreference("email");
+
+  const emailLink = useMemo(() => {
+    if (auth.isSignInWithEmailLink(window.location.href)) {
+      return window.location.href;
+    }
+  }, [auth]);
+
+  const sendLink = useCallback(
+    (email: string) => {
+      return auth.sendSignInLinkToEmail(email, {
+        url: window.location.toString(),
+        handleCodeInApp: true,
+      });
+    },
+    [auth]
   );
+
+  const signIn = useCallback(
+    (email: string, emailLink: string) => {
+      return auth.signInWithEmailLink(email, emailLink);
+    },
+    [auth]
+  );
+
+  const submit = useCallback(
+    async (cred: { email: string }) => {
+      if (emailLink) {
+        await signIn(cred.email, emailLink);
+        return;
+      }
+      await sendLink(cred.email);
+      return;
+    },
+    [signIn, sendLink, emailLink]
+  );
+
+  const submitHandle = useHandler(submit);
+
+  useEffect(() => {
+    if (email.length && emailLink) {
+      submitHandle.callback({ email });
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   const shouldPromptUser = !submitHandle.busy && !auth.currentUser;
 
