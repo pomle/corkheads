30e7de2405e2e95db66ebefba2f01fcf413dcc1a
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-12 20:41:57 +0100

    Add create button to Find page

--- app/src/components/route/routes/AppRoute/pages/FindPage/FindPage.tsx
+++ app/src/components/route/routes/AppRoute/pages/FindPage/FindPage.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useCallback } from "react";
 import NavigationBar from "components/ui/layout/NavigationBar";
 import { useHistory } from "react-router-dom";
 import { Article } from "types/types";
+import * as paths from "components/route/paths";
 import ExploreArticlesView from "components/views/ExploreArticlesView";
 
 interface FindPageProps {
@@ -11,8 +12,16 @@ interface FindPageProps {
 const FindPage: React.FC<FindPageProps> = ({ onSelect }) => {
   const history = useHistory();
 
+  const goToCreate = useCallback(() => {
+    const url = paths.articleCreate.url({});
+    history.push(url);
+  }, [history]);
+
   const nav = (
-    <NavigationBar forward={<button onClick={history.goBack}>Cancel</button>} />
+    <NavigationBar
+      back={<button onClick={history.goBack}>Cancel</button>}
+      forward={<button onClick={goToCreate}>Add Drink</button>}
+    />
   );
 
   return <ExploreArticlesView nav={nav} onSelect={onSelect} />;
