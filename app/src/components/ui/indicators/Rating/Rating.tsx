7295c70ccb76011ae8ab0360b6df53fcf5093fd4
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-13 17:58:09 +0100

    Add Rating component

---
+++ app/src/components/ui/indicators/Rating/Rating.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  rating: {
+    display: "flex",
+    fontSize: "10px",
+    justifyContent: "space-around",
+  },
+});
+
+interface RatingProps {
+  rating: number;
+}
+
+const SCALE = 5;
+const RATINGS = [1, 2, 3, 4, 5];
+
+const Rating: React.FC<RatingProps> = ({ rating }) => {
+  const classes = useStyles();
+
+  const score = Math.floor(rating * SCALE);
+
+  return (
+    <div className={classes.rating}>
+      {RATINGS.map((rating) => {
+        return (
+          <div
+            style={{
+              filter: score >= rating ? "grayscale(0%)" : "grayscale(100%)",
+            }}
+            key={rating}
+          >
+            ⭐
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Rating;
