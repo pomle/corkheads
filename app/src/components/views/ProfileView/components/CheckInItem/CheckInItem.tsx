917f64d944acc0d8372ee575a2cd56fe5d74ba88
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-13 17:58:28 +0100

    Add CheckIn item

---
+++ app/src/components/views/ProfileView/components/CheckInItem/CheckInItem.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Article, CheckIn } from "types/types";
+import Badge from "components/ui/typography/Badge";
+import Rating from "components/ui/indicators/Rating";
+
+const useStyles = makeStyles({
+  checkIn: {
+    display: "flex",
+  },
+  photo: {
+    background: "#c9c9c9",
+    overflow: "hidden",
+    height: "85px",
+    width: "85px",
+  },
+  meta: {
+    background: "#fff",
+    color: "#5a5a5a",
+    display: "grid",
+    gridAutoFlow: "row",
+    gridGap: "4px",
+    flex: "1",
+    lineHeight: 1.4,
+    padding: "14px 16px",
+  },
+  displayName: {
+    fontSize: "17px",
+    fontWeight: 700,
+  },
+  manufacturer: {
+    fontSize: "12px",
+    fontWeight: 500,
+  },
+  rating: {
+    alignItems: "center",
+    display: "flex",
+    margin: "-4px",
+    "& > *": {
+      margin: "4px",
+    },
+  },
+});
+
+interface CheckInItemProps {
+  article: Article;
+  checkIn: CheckIn;
+}
+
+const CheckInItem: React.FC<CheckInItemProps> = ({ checkIn, article }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.checkIn}>
+      <div className={classes.photo}></div>
+      <div className={classes.meta}>
+        <div>
+          <div className={classes.displayName}>{article.data.displayName}</div>
+          <div className={classes.manufacturer}>
+            {article.data.manufacturer}
+          </div>
+        </div>
+        <div className={classes.rating}>
+          <Badge>4 Check Ins</Badge>
+          {checkIn.data.rating && <Rating rating={checkIn.data.rating / 5} />}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CheckInItem;
