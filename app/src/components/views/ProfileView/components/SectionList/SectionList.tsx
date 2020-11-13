33b2ef71d0238a48cb5e45baf5bb05687da41ef4
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-13 18:21:28 +0100

    Add SectionList component

---
+++ app/src/components/views/ProfileView/components/SectionList/SectionList.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  sectionList: {
+    "& > section": {
+      marginBottom: "16px",
+    },
+  },
+});
+
+interface SectionListProps {}
+
+const SectionList: React.FC<SectionListProps> = ({ children }) => {
+  const classes = useStyles();
+  return <div className={classes.sectionList}>{children}</div>;
+};
+
+export default SectionList;
