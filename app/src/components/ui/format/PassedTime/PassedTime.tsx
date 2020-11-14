f5bc19b537d969019ee1c327a79333c6d0cdcaf7
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-15 00:49:22 +0100

    Add PassedTime component

---
+++ app/src/components/ui/format/PassedTime/PassedTime.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Moment } from "moment";
+import { useLiveTime } from "components/hooks/useLiveTime";
+import Day from "components/ui/format/Day";
+import Time from "components/ui/format/Time";
+import * as Trans from "./locales";
+
+interface PassedTimeProps {
+  date: Moment;
+}
+
+const PassedTime: React.FC<PassedTimeProps> = ({ date }) => {
+  const now = useLiveTime("minute");
+
+  const hours = now.diff(date, "hours");
+  if (hours < 1) {
+    const minutes = now.diff(date, "minute");
+    return <Trans.MinutesAgo minutes={minutes} />;
+  } else if (hours < 6) {
+    return <Trans.HoursAgo hours={hours} />;
+  } else if (hours < 12) {
+    return <Time date={date} />;
+  } else {
+    return <Day date={date} />;
+  }
+};
+
+export default PassedTime;
---
+++ app/src/components/ui/format/PassedTime/locales.tsx
@@ -0,0 +1,31 @@
+import { localize, Locale as LC } from "lib/i18n/localization";
+
+export const HoursAgo = localize<{ hours: number }>({
+  [LC.enGB]: ({ hours }) => {
+    if (hours === 1) {
+      return "1 hour ago";
+    }
+    return `${hours} timmar sedan`;
+  },
+  [LC.svSE]: ({ hours }) => {
+    if (hours === 1) {
+      return "1 timme sedan";
+    }
+    return `${hours} timmar sedan`;
+  },
+});
+
+export const MinutesAgo = localize<{ minutes: number }>({
+  [LC.enGB]: ({ minutes }) => {
+    if (minutes === 1) {
+      return "1 minute ago";
+    }
+    return `${minutes} minutes ago`;
+  },
+  [LC.svSE]: ({ minutes }) => {
+    if (minutes === 1) {
+      return "1 minut sedan";
+    }
+    return `${minutes} minuter sedan`;
+  },
+});
