479fd26877c015b0042cfbbd6c23ed6254aef8e1
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-15 22:04:57 +0100

    Implement ImageItem

--- app/src/components/views/ProfileView/components/CheckInItem/CheckInItem.tsx
+++ app/src/components/views/ProfileView/components/CheckInItem/CheckInItem.tsx
@@ -4,41 +4,9 @@ import { Article } from "types/article";
 import { CheckIn } from "types/checkIn";
 import Rating from "components/ui/indicators/Rating";
 import PassedTime from "components/ui/format/PassedTime";
-
-type StyleProps = {
-  photoURL?: string;
-};
-
-function backgroundImage({ photoURL }: StyleProps) {
-  if (photoURL) {
-    return `url(${photoURL})`;
-  }
-  return "none";
-}
+import ImageItem from "components/ui/layout/ImageItem";
 
 const useStyles = makeStyles({
-  checkIn: {
-    display: "flex",
-  },
-  photo: {
-    background: "#c9c9c9",
-    backgroundImage,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    overflow: "hidden",
-    height: "85px",
-    width: "85px",
-  },
-  meta: {
-    background: "#fff",
-    color: "#5a5a5a",
-    display: "grid",
-    flex: "1",
-    gridAutoFlow: "row",
-    gridGap: "4px",
-    lineHeight: 1,
-    padding: "14px 16px",
-  },
   displayName: {
     fontSize: "17px",
     fontWeight: 700,
@@ -70,23 +38,23 @@ interface CheckInItemProps {
 }
 
 const CheckInItem: React.FC<CheckInItemProps> = ({ checkIn, article }) => {
-  const classes = useStyles({ photoURL: article.data.photoURL });
+  const { photoURL } = article.data;
+
+  const classes = useStyles();
+
   return (
-    <div className={classes.checkIn}>
-      <div className={classes.photo}></div>
-      <div className={classes.meta}>
-        <div className={classes.displayName}>{article.data.displayName}</div>
-        <div className={classes.manufacturer}>{article.data.manufacturer}</div>
-        <div className={classes.timestamp}>
-          {checkIn.data.timestamp && (
-            <PassedTime date={checkIn.data.timestamp} />
-          )}
-        </div>
-        <div className={classes.rating}>
-          {checkIn.data.rating && <Rating rating={checkIn.data.rating / 5} />}
-        </div>
+    <ImageItem
+      image={photoURL && <img src={photoURL} alt={article.data.displayName} />}
+    >
+      <div className={classes.displayName}>{article.data.displayName}</div>
+      <div className={classes.manufacturer}>{article.data.manufacturer}</div>
+      <div className={classes.timestamp}>
+        {checkIn.data.timestamp && <PassedTime date={checkIn.data.timestamp} />}
       </div>
-    </div>
+      <div className={classes.rating}>
+        {checkIn.data.rating && <Rating rating={checkIn.data.rating / 5} />}
+      </div>
+    </ImageItem>
   );
 };
 
--- app/src/components/views/ProfileView/components/TopArticleItem/TopArticleItem.tsx
+++ app/src/components/views/ProfileView/components/TopArticleItem/TopArticleItem.tsx
@@ -3,41 +3,9 @@ import { makeStyles } from "@material-ui/styles";
 import Badge from "components/ui/typography/Badge";
 import { UserArticle } from "types/userArticle";
 import { Article } from "types/article";
-
-type StyleProps = {
-  photoURL?: string;
-};
-
-function backgroundImage({ photoURL }: StyleProps) {
-  if (photoURL) {
-    return `url(${photoURL})`;
-  }
-  return "none";
-}
+import ImageItem from "components/ui/layout/ImageItem";
 
 const useStyles = makeStyles({
-  TopArticleItem: {
-    display: "flex",
-  },
-  photo: {
-    background: "#c9c9c9",
-    backgroundImage,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    overflow: "hidden",
-    height: "85px",
-    width: "85px",
-  },
-  meta: {
-    background: "#fff",
-    color: "#5a5a5a",
-    display: "grid",
-    gridAutoFlow: "row",
-    gridGap: "4px",
-    flex: "1",
-    lineHeight: 1.4,
-    padding: "14px 16px",
-  },
   displayName: {
     fontSize: "17px",
     fontWeight: 700,
@@ -57,23 +25,21 @@ const TopArticleItem: React.FC<TopArticleItemProps> = ({
   article,
   userArticle,
 }) => {
-  const classes = useStyles({ photoURL: article.data.photoURL });
+  const { data } = article;
+  const { photoURL } = data;
+
+  const classes = useStyles();
 
   return (
-    <div className={classes.TopArticleItem}>
-      <div className={classes.photo} />
-      <div className={classes.meta}>
-        <div>
-          <div className={classes.displayName}>{article.data.displayName}</div>
-          <div className={classes.manufacturer}>
-            {article.data.manufacturer}
-          </div>
-        </div>
-        <div>
-          <Badge>{userArticle.data.checkIns} Check-Ins</Badge>
-        </div>
+    <ImageItem
+      image={photoURL && <img src={photoURL} alt={data.displayName} />}
+    >
+      <div className={classes.displayName}>{article.data.displayName}</div>
+      <div className={classes.manufacturer}>{article.data.manufacturer}</div>
+      <div>
+        <Badge>{userArticle.data.checkIns} Check-Ins</Badge>
       </div>
-    </div>
+    </ImageItem>
   );
 };
 
