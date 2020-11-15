a228b201df5c4129efed5b19ed6dc469ffdda088
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-15 22:04:57 +0100

    Implement ImageItem

--- app/src/components/views/ExploreArticlesView/components/ArticleItem/ArticleItem.tsx
+++ app/src/components/views/ExploreArticlesView/components/ArticleItem/ArticleItem.tsx
@@ -1,41 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Article } from "types/article";
-
-type StyleProps = {
-  photoURL?: string;
-};
-
-function backgroundImage({ photoURL }: StyleProps) {
-  if (photoURL) {
-    return `url(${photoURL})`;
-  }
-  return "none";
-}
+import ImageItem from "components/ui/layout/ImageItem";
 
 const useStyles = makeStyles({
-  ArticleItem: {
-    display: "flex",
-  },
-  photo: {
-    background: "#c9c9c9",
-    backgroundImage,
-    backgroundPosition: "center",
-    backgroundSize: "cover",
-    overflow: "hidden",
-    height: "85px",
-    width: "85px",
-  },
-  meta: {
-    background: "#fff",
-    color: "#5a5a5a",
-    display: "grid",
-    gridAutoFlow: "row",
-    gridGap: "4px",
-    flex: "1",
-    lineHeight: 1.4,
-    padding: "14px 16px",
-  },
   displayName: {
     fontSize: "17px",
     fontWeight: 700,
@@ -51,20 +19,17 @@ interface ArticleItemProps {
 }
 
 const ArticleItem: React.FC<ArticleItemProps> = ({ article }) => {
-  const classes = useStyles({ photoURL: article.data.photoURL });
+  const { photoURL } = article.data;
+
+  const classes = useStyles();
 
   return (
-    <div className={classes.ArticleItem}>
-      <div className={classes.photo} />
-      <div className={classes.meta}>
-        <div>
-          <div className={classes.displayName}>{article.data.displayName}</div>
-          <div className={classes.manufacturer}>
-            {article.data.manufacturer}
-          </div>
-        </div>
-      </div>
-    </div>
+    <ImageItem
+      image={photoURL && <img src={photoURL} alt={article.data.displayName} />}
+    >
+      <div className={classes.displayName}>{article.data.displayName}</div>
+      <div className={classes.manufacturer}>{article.data.manufacturer}</div>
+    </ImageItem>
   );
 };
 
