90d708ecc3463b61b00689ec768b65564e806bbc
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-16 02:49:05 +0100

    Add useUserUpload hook

---
+++ app/src/components/hooks/useUserUpload.ts
@@ -0,0 +1,19 @@
+import { useCallback } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { useStorage } from "components/hooks/useStorage";
+import { User } from "types/user";
+
+export function useUserUpload() {
+  const storage = useStorage();
+
+  const uploadFile = useCallback(
+    (user: User, file: File) => {
+      const fileId = uuidv4();
+      const fileRef = storage.ref(user.uid).child(fileId);
+      return fileRef.put(file);
+    },
+    [storage]
+  );
+
+  return uploadFile;
+}
