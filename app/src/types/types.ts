466a7480a77bd0563107a9fcd349ec3c11d3b980
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-17 22:37:49 +0100

    Add clone function to types

--- app/src/types/types.ts
+++ app/src/types/types.ts
@@ -9,3 +9,12 @@ export interface Converter<T> {
   toFirestore(data: Container<T>): firebase.firestore.DocumentData;
   fromFirestore(snapshot: Snapshot): Container<T>;
 }
+
+export function clone<T>(container: Container<T>): Container<T> {
+  return {
+    id: container.id,
+    data: {
+      ...container.data,
+    },
+  };
+}
