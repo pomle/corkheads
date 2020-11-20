fb4ce5ec70e5199a281443d5f4829ca7025a22ef
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-20 07:10:16 +0100

    Decouple search index functions to module

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,5 @@
-import * as functions from "firebase-functions";
-import algoliasearch from "algoliasearch";
-// Initialize Algolia, requires installing Algolia dependencies:
-// https://www.algolia.com/doc/api-client/javascript/getting-started/#install
-//
-// App ID and API Key are stored in functions config variables
-const ALGOLIA_ID = functions.config().algolia.app_id;
-const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
+import * as search from "./search/algolia";
 
-const ALGOLIA_INDEX_NAME = "articles";
-const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
-
-exports.onArticleCreated = functions.firestore
-  .document("articles/{articleId}")
-  .onCreate((snap, context) => {
-    const data = snap.data();
-    data.objectID = context.params.articleId;
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.saveObject(data);
-  });
-
-exports.onArticleUpdated = functions.firestore
-  .document("articles/{articleId}")
-  .onUpdate((snap, context) => {
-    const data = snap.after.data();
-    data.objectID = context.params.articleId;
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.saveObject(data);
-  });
-
-exports.onArticleDeleted = functions.firestore
-  .document("articles/{articleId}")
-  .onDelete((snap, context) => {
-    const objectID = context.params.articleId;
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.deleteObject(objectID);
-  });
+exports.onArticleCreated = search.onArticleCreated;
+exports.onArticleUpdated = search.onArticleUpdated;
+exports.onArticleDeleted = search.onArticleDeleted;
