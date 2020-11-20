14eb5aded5804d2bc9f82ceae401570f60b2c953
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-20 06:34:44 +0100

    Format Cloud functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-import * as functions from 'firebase-functions';
+import * as functions from "firebase-functions";
 import algoliasearch from "algoliasearch";
 // Initialize Algolia, requires installing Algolia dependencies:
 // https://www.algolia.com/doc/api-client/javascript/getting-started/#install
@@ -7,25 +7,31 @@ import algoliasearch from "algoliasearch";
 const ALGOLIA_ID = functions.config().algolia.app_id;
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
 
-const ALGOLIA_INDEX_NAME = 'articles';
+const ALGOLIA_INDEX_NAME = "articles";
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
 
-exports.onArticleCreated = functions.firestore.document('articles/{articleId}').onCreate((snap, context) => {
+exports.onArticleCreated = functions.firestore
+  .document("articles/{articleId}")
+  .onCreate((snap, context) => {
     const data = snap.data();
     data.objectID = context.params.articleId;
     const index = client.initIndex(ALGOLIA_INDEX_NAME);
     return index.saveObject(data);
-});
+  });
 
-exports.onArticleUpdated = functions.firestore.document('articles/{articleId}').onUpdate((snap, context) => {
+exports.onArticleUpdated = functions.firestore
+  .document("articles/{articleId}")
+  .onUpdate((snap, context) => {
     const data = snap.after.data();
     data.objectID = context.params.articleId;
     const index = client.initIndex(ALGOLIA_INDEX_NAME);
     return index.saveObject(data);
-});
+  });
 
-exports.onArticleDeleted = functions.firestore.document('articles/{articleId}').onDelete((snap, context) => {
+exports.onArticleDeleted = functions.firestore
+  .document("articles/{articleId}")
+  .onDelete((snap, context) => {
     const objectID = context.params.articleId;
     const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.deleteObject(objectID)
-});
+    return index.deleteObject(objectID);
+  });
