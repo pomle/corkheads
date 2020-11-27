902f2b6eea97966c7cec124ddf399430d31f69c9
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-27 17:32:10 +0100

    Add image format lib

---
+++ app/src/lib/imageFormat.ts
@@ -0,0 +1,34 @@
+import { shrinkToFitRect } from "./math";
+import { createBlob, loadImage, resize, Size } from "./image";
+
+export function loadFileAsImage(file: File): Promise<HTMLImageElement> {
+  const fileURL = URL.createObjectURL(file);
+  const imagePromise = loadImage(fileURL);
+  imagePromise.finally(() => {
+    URL.revokeObjectURL(fileURL);
+  });
+  return imagePromise;
+}
+
+export function downscale(inputImage: HTMLImageElement, size: Size) {
+  const FORMAT = "image/jpeg";
+  const QUALITY = 0.9;
+
+  const outputSize = shrinkToFitRect(
+    {
+      x: inputImage.width,
+      y: inputImage.height,
+    },
+    {
+      x: size.width,
+      y: size.height,
+    }
+  );
+
+  const outputImage = resize(inputImage, {
+    width: outputSize.x,
+    height: outputSize.y,
+  });
+
+  return createBlob(outputImage, FORMAT, QUALITY);
+}
