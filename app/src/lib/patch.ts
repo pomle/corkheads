ab22ca68d56b03457bd234c8b034712877484ec1
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-30 22:48:06 +0100

    Fix bugs in and add test for Patch

--- app/src/lib/patch.ts
+++ app/src/lib/patch.ts
@@ -1,22 +1,28 @@
 import { Bottling, createBottling } from "types/Bottling";
 import merge from "deepmerge";
-import { updatedDiff } from "deep-object-diff";
+import { addedDiff, updatedDiff } from "deep-object-diff";
 import { Article } from "types/Article";
 import { UserArticle } from "types/UserArticle";
+
+const DEFAULT_BOTTLING = createBottling();
 
 export function mergeBottling(bottlings: Bottling[]): Bottling {
   return merge.all(bottlings) as Bottling;
 }
 
-export function diffBottling(a: Bottling, b: Bottling): Bottling {
-  return updatedDiff(a, b) as Bottling;
+export function diffBottling(
+  a: Partial<Bottling>,
+  b: Partial<Bottling>
+): Partial<Bottling> {
+  const x = createBottling();
+  return merge.all([addedDiff(x, b), updatedDiff(a, b)]);
 }
 
 export function getPreferredBottling(
   article?: Article,
   userArticle?: UserArticle
 ) {
-  const bottlings: Bottling[] = [createBottling()];
+  const bottlings: Bottling[] = [DEFAULT_BOTTLING];
   if (article?.bottling) {
     bottlings.push(article.bottling);
   }
@@ -24,4 +30,16 @@ export function getPreferredBottling(
     bottlings.push(userArticle.bottling);
   }
   return mergeBottling(bottlings);
+}
+
+export function getEffectiveBottlingChanges(
+  bottling: Partial<Bottling>,
+  article: Article
+) {
+  const bottlings = [DEFAULT_BOTTLING];
+  if (article.bottling) {
+    bottlings.push(article.bottling);
+  }
+  const sourceBottling = mergeBottling(bottlings);
+  return diffBottling(sourceBottling, bottling);
 }
