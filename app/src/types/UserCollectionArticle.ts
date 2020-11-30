2c308f55bd0d66aed781ba90a5b5e73470f76437
Pontus Alexander <pontus.alexander@gmail.com>
2020-11-30 07:37:28 +0100

    Fix Firebase import to mitigate app size

--- app/src/types/UserCollectionArticle.ts
+++ app/src/types/UserCollectionArticle.ts
@@ -1,4 +1,4 @@
-import { firestore } from "firebase";
+import { firestore } from "firebase/app";
 import moment, { Moment } from "moment";
 import { createConverter } from "lib/firestore/converter";
 import { toMoment } from "./convert";
