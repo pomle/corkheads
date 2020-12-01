15a4ae4949030afe90ecf26f19e5d459a246dfda
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-01 07:25:38 +0100

    Remove ArticleSelect from Explore page

--- app/src/components/views/ExploreArticlesView/ExploreArticlesView.tsx
+++ app/src/components/views/ExploreArticlesView/ExploreArticlesView.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import ViewTitle from "components/ui/layout/ViewTitle";
+import { makeStyles } from "@material-ui/styles";
 import HeaderLayout from "components/ui/layout/HeaderLayout";
 import ViewCap from "components/ui/layout/ViewCap";
 import ViewBody from "components/ui/layout/ViewBody";
 import * as Locales from "./locales";
-import ArticleSelect from "components/fragments/Article/ArticleSelect";
 import { Article } from "types/Article";
+import { useSharedInput } from "components/hooks/useSharedInput";
+import SearchInput from "components/ui/input/SearchInput";
+import ArticleResults from "components/fragments/Article/ArticleResults";
+import ResultStatement from "components/ui/typography/ResultStatement";
+import ViewHead from "components/ui/layout/ViewHead";
+
+const MIN_QUERY_LENGTH = 3;
+
+const useStyles = makeStyles({
+  ArticleSelect: {},
+  searchBar: {
+    margin: "24px",
+  },
+  searchResults: {},
+});
 
 interface ExploreArticlesViewProps {
   nav: React.ReactNode;
@@ -16,14 +30,31 @@ const ExploreArticlesView: React.FC<ExploreArticlesViewProps> = ({
   nav,
   onSelect,
 }) => {
+  const [query, setQuery] = useSharedInput("site-select-query", "");
+
+  const classes = useStyles();
+
   return (
     <HeaderLayout>
       <ViewCap top>
         {nav}
-        <ViewTitle title={<Locales.FindDrink />} />
+        <ViewHead>
+          <h1>
+            <Locales.FindDrink />
+          </h1>
+        </ViewHead>
       </ViewCap>
       <ViewBody>
-        <ArticleSelect onSelect={onSelect} />
+        <div className={classes.searchBar}>
+          <SearchInput query={query} onChange={setQuery} />
+        </div>
+        <div className={classes.searchResults}>
+          {query.length >= MIN_QUERY_LENGTH ? (
+            <ArticleResults query={query} onSelect={onSelect} />
+          ) : (
+            <ResultStatement message="Type in the name of a drink or manufacturer." />
+          )}
+        </div>
       </ViewBody>
     </HeaderLayout>
   );
