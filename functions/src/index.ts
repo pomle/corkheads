ddbc9f253d50fd5925f8cd17813ea96ad2378893
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-02 08:36:05 +0100

    Enable collection aggregates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,12 @@
 import * as search from "./search/algolia";
 import { aggregateArticle } from "./rating/aggregateArticle";
+import { collectionSizeAggregate, wishlistSizeAggregate } from "./user/counts";
 
 exports.onArticleCreated = search.onArticleCreated;
 exports.onArticleUpdated = search.onArticleUpdated;
 exports.onArticleDeleted = search.onArticleDeleted;
 
 exports.aggregateArticle = aggregateArticle;
+
+exports.collectionSizeAggregate = collectionSizeAggregate;
+exports.wishlistSizeAggregate = wishlistSizeAggregate;
