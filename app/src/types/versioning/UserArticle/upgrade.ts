fcade69fda0e647b48cf00b8ae3849d3a621b2db
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-05 13:31:25 +0100

    WIP Investigate Rating fix

--- app/src/types/versioning/UserArticle/upgrade.ts
+++ app/src/types/versioning/UserArticle/upgrade.ts
@@ -1,10 +1,21 @@
+import Rating from "components/ui/indicators/Rating";
 import { UserArticle } from "types/UserArticle";
 import { upgradeRatingV0 } from "../Rating/upgrade";
 import { v1, v2 } from "./revisions";
 
-function upgradeV1(source: v2 | v1): UserArticle {
-  let output = { ...source } as any;
+function hasV2Rating(source: any) {
+  if ("score" in source?.rating && "love" in source?.rating) {
+    return true;
+  }
+  return false;
+}
 
+function upgradeV1(source: v2 | v1): UserArticle {
+  if (hasV2Rating(source)) {
+    return source as UserArticle;
+  }
+
+  let output = { ...source } as any;
   if ("loveIt" in output) {
     output.rating = upgradeRatingV0(output);
     delete output.loveIt;
