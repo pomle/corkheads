22dce7f86f8c47404c3e06f170850dd7f8cb0240
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-05 10:11:28 +0100

    Send environment to Sentry

--- app/src/sentry.ts
+++ app/src/sentry.ts
@@ -6,6 +6,7 @@ import appConfig from "config/app.config.js";
 export function initSentry() {
   Sentry.init({
     dsn: config.dsn,
+    environment: appConfig.environment,
     release: "corkheads@" + appConfig.version,
     integrations: [new Integrations.BrowserTracing()],
     tracesSampleRate: 1.0,
