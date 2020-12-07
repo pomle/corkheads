52c82530dbb6ff5fa035c6ff64e0393a493a9cd2
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-07 08:01:56 +0100

    Show Check ins on ArticleDetails page

---
+++ app/src/components/views/ArticleDetailsView/components/ArticleCheckInsSection/ArticleCheckInsSection.tsx
@@ -0,0 +1,74 @@
+import React, { useCallback, useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import {
+  CheckInQuery,
+  useCheckInQuery,
+} from "components/hooks/db/useCheckInQuery";
+import ItemList from "components/ui/layout/ItemList";
+import CheckInItem from "components/fragments/CheckIn/CheckInItem";
+import SectionList from "components/ui/layout/SectionList/SectionList";
+import Section from "components/ui/layout/Section";
+import SectionTitle from "components/ui/layout/SectionTitle";
+import * as paths from "components/route/paths";
+
+interface ArticleCheckInsSectionProps {
+  articleId: string;
+}
+
+const ArticleCheckInsSection: React.FC<ArticleCheckInsSectionProps> = ({
+  articleId,
+}) => {
+  const history = useHistory();
+
+  const goToCheckIn = useCallback(
+    (checkInId: string) => {
+      const url = paths.checkInView.url({ checkInId });
+      history.push(url);
+    },
+    [history]
+  );
+
+  const query = useMemo((): CheckInQuery => {
+    return {
+      filters: {
+        articleIds: [articleId],
+      },
+      order: [
+        {
+          field: "timestamp",
+          dir: "desc",
+        },
+      ],
+      limit: 10,
+    };
+  }, [articleId]);
+
+  const result = useCheckInQuery(query);
+
+  return (
+    <SectionList>
+      <Section header={<SectionTitle main="Check ins" />}>
+        <ItemList>
+          {result &&
+            result.map(({ checkInEntry, articleEntry }) => {
+              const article = articleEntry.data;
+              const checkIn = checkInEntry.data;
+
+              return (
+                <button
+                  key={checkInEntry.id}
+                  onClick={() => goToCheckIn(checkInEntry.id)}
+                >
+                  {article && checkIn && (
+                    <CheckInItem checkIn={checkIn} article={article} />
+                  )}
+                </button>
+              );
+            })}
+        </ItemList>
+      </Section>
+    </SectionList>
+  );
+};
+
+export default ArticleCheckInsSection;
