30a3e66b7068a453dd91099d607a1e0acdb8d31c
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-08 12:39:26 +0100

    Refactor UserSections in ArticleDetailsView

--- app/src/components/views/ArticleDetailsView/components/UserSections/UserSections.tsx
+++ app/src/components/views/ArticleDetailsView/components/UserSections/UserSections.tsx
@@ -1,19 +1,13 @@
-import React, { useLayoutEffect, useMemo, useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import { makeStyles } from "@material-ui/styles";
 import SectionList from "components/ui/layout/SectionList";
 import Section from "components/ui/layout/Section";
 import SectionTitle from "components/ui/layout/SectionTitle";
 import BottlingUserInput from "components/fragments/Bottling/BottlingUserInput";
-import { Bottling } from "types/Bottling";
 import InventoryUserInput from "components/fragments/Inventory/InventoryUserInput";
-import { Inventory } from "types/Inventory";
-import { debounce } from "lib/debounce";
-import { useArticle } from "components/hooks/db/useArticles";
-import { useUserArticle } from "components/hooks/db/useUserArticles";
-import { useUserCollectionArticle } from "components/hooks/db/useUserCollectionArticles";
-import { useUserArticleBottlingUpdate } from "components/hooks/db/useUserArticleBottlingUpdate";
-import { getPreferredBottling } from "lib/patch";
 import Themer from "components/ui/theme/Themer";
+import { useBottling, useInventory } from "./hooks";
+import { useUserCollectionArticle } from "components/hooks/db/useUserCollectionArticles";
 
 type StyleProps = {
   folded: number;
@@ -32,8 +26,6 @@ const useStyles = makeStyles({
     padding: "0 24px 16px 24px",
   },
 });
-
-const STORE_DELAY = 5000;
 
 interface UserSectionsProps {
   userId: string;
@@ -57,47 +49,16 @@ const UserSections: React.FC<UserSectionsProps> = ({ userId, articleId }) => {
     setCollapse(heightDiff);
   }, []);
 
-  const articleEntry = useArticle(articleId);
-  const userArticleEntry = useUserArticle(userId, articleId);
   const userCollectionArticleEntry = useUserCollectionArticle(
     userId,
     articleId
   );
 
-  const article = articleEntry?.data;
-  const userArticle = userArticleEntry?.data;
-  const userCollectionArticle = userCollectionArticleEntry?.data;
-
-  const bottling = useMemo((): Bottling => {
-    return getPreferredBottling(article, userArticle);
-  }, [article, userArticle]);
-
-  const updateBottling = useUserArticleBottlingUpdate(userId, articleId);
-  const handleChangeBottling = useMemo(() => {
-    return debounce(updateBottling, STORE_DELAY);
-  }, [updateBottling]);
-
-  const inventory = useMemo((): Inventory => {
-    return userCollectionArticle?.inventory || {};
-  }, [userCollectionArticle]);
-
-  const handleChangeInventory = useMemo(() => {
-    if (!userCollectionArticleEntry) {
-      return;
-    }
-
-    return debounce((inventory: Inventory) => {
-      userCollectionArticleEntry.doc.set(
-        {
-          inventory,
-        },
-        { mergeFields: ["inventory"] }
-      );
-    }, STORE_DELAY);
-  }, [userCollectionArticleEntry]);
+  const { bottling, handleBottlingChange } = useBottling(userId, articleId);
+  const { inventory, handleInventoryChange } = useInventory(userId, articleId);
 
   const classes = useStyles({
-    folded: userCollectionArticle?.active ? 0 : collapse,
+    folded: !userCollectionArticleEntry?.data?.active ? collapse : 0,
   });
 
   return (
@@ -107,12 +68,10 @@ const UserSections: React.FC<UserSectionsProps> = ({ userId, articleId }) => {
           <div className="collection">
             <Section header={<SectionTitle main="Your Collection" />}>
               <div className={classes.section}>
-                {inventory && handleChangeInventory && (
-                  <InventoryUserInput
-                    inventory={inventory}
-                    onChange={handleChangeInventory}
-                  />
-                )}
+                <InventoryUserInput
+                  inventory={inventory}
+                  onChange={handleInventoryChange}
+                />
               </div>
             </Section>
           </div>
@@ -120,12 +79,10 @@ const UserSections: React.FC<UserSectionsProps> = ({ userId, articleId }) => {
           <div className="about">
             <Section header={<SectionTitle main="About this bottling" />}>
               <div className={classes.section}>
-                {bottling && handleChangeBottling && (
-                  <BottlingUserInput
-                    bottling={bottling}
-                    onChange={handleChangeBottling}
-                  />
-                )}
+                <BottlingUserInput
+                  bottling={bottling}
+                  onChange={handleBottlingChange}
+                />
               </div>
             </Section>
           </div>
