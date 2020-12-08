0eac7d42fa88688ffe4ed2dc77f0ae9198e238cf
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-08 08:06:51 +0100

    Make ErrorBoundary PureComponent

--- app/src/components/views/ErrorBoundaryView/ErrorBoundaryView.tsx
+++ app/src/components/views/ErrorBoundaryView/ErrorBoundaryView.tsx
@@ -18,7 +18,7 @@ interface ErrorBoundaryState {
   error?: Error;
 }
 
-class ErrorBoundary extends React.Component<
+class ErrorBoundary extends React.PureComponent<
   ErrorBoundaryProps,
   ErrorBoundaryState
 > {
