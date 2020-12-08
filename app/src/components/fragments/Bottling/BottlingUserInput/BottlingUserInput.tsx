4dbe020aadd5889299d17bb85db6ecf0eedce97f
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-08 20:16:09 +0100

    Style and categorize Whisky input

--- app/src/components/fragments/Bottling/BottlingUserInput/BottlingUserInput.tsx
+++ app/src/components/fragments/Bottling/BottlingUserInput/BottlingUserInput.tsx
@@ -1,9 +1,18 @@
 import React, { useMemo } from "react";
+import { makeStyles } from "@material-ui/styles";
 import EntryList from "components/ui/layout/EntryList";
 import Entry from "components/ui/layout/Entry";
 import { useUserInput } from "components/hooks/useUserInput";
 import { Bottling } from "types/Bottling";
 import { toBottling, toEntries } from "./conversion";
+
+const useStyles = makeStyles({
+  BottlingUserInput: {
+    "& section h4": {
+      margin: "32px 0 8px 0",
+    },
+  },
+});
 
 interface BottlingUserInputProps {
   bottling: Bottling;
@@ -22,108 +31,179 @@ const BottlingUserInput: React.FC<BottlingUserInputProps> = ({
     onChange(toBottling(entries));
   });
 
+  const classes = useStyles();
+
   return (
-    <EntryList>
-      <Entry name="Bottles produced">
-        <input
-          type="number"
-          placeholder="# of bottles"
-          {...userInput.bottleCount}
-        />
-      </Entry>
+    <div className={classes.BottlingUserInput}>
+      <section>
+        <EntryList>
+          <Entry name="Label">
+            <input
+              type="text"
+              placeholder="Label name"
+              {...userInput.bottleLabel}
+            />
+          </Entry>
 
-      <Entry name="Label">
-        <input
-          type="text"
-          placeholder="Label name"
-          {...userInput.bottleLabel}
-        />
-      </Entry>
+          <Entry name="Series">
+            <input
+              type="text"
+              placeholder="Series name"
+              {...userInput.series}
+            />
+          </Entry>
 
-      <Entry name="Series">
-        <input type="text" placeholder="Series name" {...userInput.series} />
-      </Entry>
+          <Entry name="Distillery">
+            <input
+              type="text"
+              placeholder="Name of distiller"
+              {...userInput.distillerName}
+            />
+          </Entry>
 
-      <Entry name="Distilled year">
-        <input type="number" placeholder="Year" {...userInput.distillYear} />
-      </Entry>
+          <Entry name="Bottled by">
+            <input
+              type="text"
+              placeholder="Name of bottler"
+              {...userInput.bottlerName}
+            />
+          </Entry>
 
-      <Entry name="Distilled by">
-        <input
-          type="text"
-          placeholder="Distillery name"
-          {...userInput.distillerName}
-        />
-      </Entry>
+          <Entry name="Age">
+            <input type="number" placeholder="# of years" {...userInput.age} />
+          </Entry>
 
-      <Entry name="Distiller country">
-        <input
-          type="text"
-          placeholder="Country"
-          {...userInput.distillerCountry}
-        />
-      </Entry>
+          <Entry name="Alcohol">
+            <input type="number" placeholder="%" {...userInput.abv} />
+          </Entry>
+        </EntryList>
+      </section>
 
-      <Entry name="Distiller district">
-        <input
-          type="text"
-          placeholder="District"
-          {...userInput.distillerDistrict}
-        />
-      </Entry>
+      <section>
+        <h4>The Whisky</h4>
 
-      <Entry name="Bottled by">
-        <input
-          type="text"
-          placeholder="Bottler name"
-          {...userInput.bottlerName}
-        />
-      </Entry>
+        <EntryList>
+          <Entry name="Date distilled">
+            <input
+              type="number"
+              placeholder="Year"
+              {...userInput.distillYear}
+            />
+          </Entry>
 
-      <Entry name="Bottle year">
-        <input type="number" placeholder="Year" {...userInput.bottlingYear} />
-      </Entry>
+          <Entry name="Date bottled">
+            <input
+              type="number"
+              placeholder="Year"
+              {...userInput.bottlingYear}
+            />
+          </Entry>
 
-      <Entry name="Bottle sizes">
-        <input
-          type="text"
-          placeholder="Sizes of bottles"
-          {...userInput.bottleSize}
-        />
-      </Entry>
+          <Entry name="Age">
+            <input type="number" placeholder="# of years" {...userInput.age} />
+          </Entry>
 
-      <Entry name="Aged">
-        <input type="number" placeholder="# of years" {...userInput.age} />
-      </Entry>
+          <Entry name="Cask No.">
+            <input
+              type="text"
+              placeholder="Cask serial no."
+              {...userInput.distillCaskNo}
+            />
+          </Entry>
 
-      <Entry name="Alcohol">
-        <input type="number" placeholder="%" {...userInput.abv} />
-      </Entry>
+          <Entry name="Cask type">
+            <input
+              type="text"
+              placeholder="Olorosso, Bourbon"
+              {...userInput.distillCaskType}
+            />
+          </Entry>
 
-      <Entry name="Distill Cask No.">
-        <input
-          type="text"
-          placeholder="Cask serial no."
-          {...userInput.distillCaskNo}
-        />
-      </Entry>
+          <Entry name="Batch No.">
+            <input
+              type="text"
+              placeholder="Batch serial no."
+              {...userInput.distillBatchNo}
+            />
+          </Entry>
 
-      <Entry name="Distill Cask Type">
-        <input
-          type="text"
-          placeholder="Olorosso, Bourbon"
-          {...userInput.distillCaskType}
-        />
-      </Entry>
+          <Entry name="Alcohol">
+            <input type="number" placeholder="%" {...userInput.abv} />
+          </Entry>
+        </EntryList>
+      </section>
 
-      <Entry name="Distill Batch No.">
-        <input
-          type="text"
-          placeholder="Batch serial no."
-          {...userInput.distillBatchNo}
-        />
-      </Entry>
-    </EntryList>
+      <section>
+        <h4>The Distillery</h4>
+        <EntryList>
+          <Entry name="Distilled by">
+            <input
+              type="text"
+              placeholder="Distillery name"
+              {...userInput.distillerName}
+            />
+          </Entry>
+
+          <Entry name="Country">
+            <input
+              type="text"
+              placeholder="ex. Scotland"
+              {...userInput.distillerCountry}
+            />
+          </Entry>
+
+          <Entry name="District">
+            <input
+              type="text"
+              placeholder="ex. Islay"
+              {...userInput.distillerDistrict}
+            />
+          </Entry>
+        </EntryList>
+      </section>
+
+      <section>
+        <h4>The Bottle</h4>
+
+        <EntryList>
+          <Entry name="Bottled by">
+            <input
+              type="text"
+              placeholder="Name of bottler"
+              {...userInput.bottlerName}
+            />
+          </Entry>
+
+          <Entry name="Bottles produced">
+            <input
+              type="number"
+              placeholder="# of bottles"
+              {...userInput.bottleCount}
+            />
+          </Entry>
+
+          <Entry name="Bottle sizes">
+            <input
+              type="text"
+              placeholder="ex. 70cl, 40cl"
+              {...userInput.bottleSize}
+            />
+          </Entry>
+
+          <Entry name="Bottle code">
+            <input
+              type="text"
+              placeholder="ex. L6 3224"
+              {...userInput.bottleCode}
+            />
+          </Entry>
+
+          <Entry name="Barcode">
+            <input type="number" placeholder="ex. 312541512" />
+          </Entry>
+        </EntryList>
+      </section>
+    </div>
   );
 };
 
