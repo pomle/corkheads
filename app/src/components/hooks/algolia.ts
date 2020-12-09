726a582c49d62b92243547e1be438acb0009616e
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-09 15:51:39 +0100

    Refactor search

--- app/src/components/hooks/algolia.ts
+++ app/src/components/hooks/algolia.ts
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { createClient } from "lib/api/algolia";
 
 export function useAlgolia() {
@@ -8,4 +8,44 @@ export function useAlgolia() {
 export function useArticleIndex() {
   const client = useAlgolia();
   return useMemo(() => client.initIndex("articles"), [client]);
+}
+
+const SEARCH_TYPE_DEBOUNCE = 250;
+
+export function useSearch() {
+  const [busy, setBusy] = useState<string>();
+  const searchIndex = useArticleIndex();
+
+  const search = useMemo(() => {
+    let timer: NodeJS.Timeout;
+
+    return (query: string): ReturnType<typeof searchIndex.search> => {
+      clearTimeout(timer);
+
+      setBusy(query);
+
+      return new Promise((resolve) => {
+        const execute = () => {
+          searchIndex
+            .search(query)
+            .then((results) => {
+              resolve(results);
+            })
+            .finally(() => {
+              setBusy((state) => (state === query ? undefined : state));
+            });
+        };
+
+        timer = setTimeout(execute, SEARCH_TYPE_DEBOUNCE);
+      });
+    };
+  }, [searchIndex]);
+
+  return useMemo(
+    () => ({
+      busy: !!busy,
+      search,
+    }),
+    [busy, search]
+  );
 }
