c012c154d96188f597bd928803dc013e01c60ba3
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-12 19:21:31 +0100

    Fix photo backnav and introduce the idea of having flat page hierarchy and injecting routes.

--- app/src/components/route/routes/ArticleRoutes/ArticleRoutes.tsx
+++ app/src/components/route/routes/ArticleRoutes/ArticleRoutes.tsx
@@ -1,23 +1,39 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { useHistory } from "react-router-dom";
 import Screen from "components/route/Screen";
 import ViewStack from "components/ui/layout/ViewStack";
 import { SlideRight } from "components/ui/transitions/Slide";
 import { ZoomCenter } from "components/ui/transitions/Zoom";
+import ArticlePicturePage from "components/route/pages/ArticlePicturePage";
 import * as paths from "components/route/paths";
 import ArticlePage from "./pages/ArticlePage";
 import CheckInCreatePage from "./pages/CheckInCreatePage";
-import ArticlePicturePage from "./pages/ArticlePicturePage";
+import * as rootPaths from "../../paths";
 
 interface ArticleRoutesProps {
   articleId: string;
 }
 
 const ArticleRoutes: React.FC<ArticleRoutesProps> = ({ articleId }) => {
+  const history = useHistory();
+
+  const routes = useMemo(
+    () => ({
+      back: () => {
+        const url = rootPaths.articleView.url({ articleId });
+        history.push(url);
+      },
+    }),
+    [articleId, history]
+  );
+
   return (
     <ViewStack>
       <ArticlePage articleId={articleId} />
       <Screen path={paths.articlePicture} transition={ZoomCenter}>
-        {(params) => <ArticlePicturePage articleId={params.articleId} />}
+        {(params) => (
+          <ArticlePicturePage routes={routes} articleId={params.articleId} />
+        )}
       </Screen>
       <Screen path={paths.articleCheckIn} transition={SlideRight}>
         {(params) => <CheckInCreatePage articleId={params.articleId} />}
