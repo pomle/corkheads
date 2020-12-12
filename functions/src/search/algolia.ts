d5ac79dfc6fda89631adf3229bb2ea055705307b
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-12 13:43:37 +0100

    Reuse index instance

--- functions/src/search/algolia.ts
+++ functions/src/search/algolia.ts
@@ -7,16 +7,15 @@ import algoliasearch from "algoliasearch";
 const ALGOLIA_ID = functions.config().algolia.app_id;
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key;
 
-const ALGOLIA_INDEX_NAME = "articles";
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY);
+const articlesIndex = client.initIndex("articles");
 
 export const onArticleCreated = functions.firestore
   .document("articles/{articleId}")
   .onCreate((snap, context) => {
     const data = snap.data();
     data.objectID = context.params.articleId;
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.saveObject(data);
+    return articlesIndex.saveObject(data);
   });
 
 export const onArticleUpdated = functions.firestore
@@ -24,14 +23,12 @@ export const onArticleUpdated = functions.firestore
   .onUpdate((snap, context) => {
     const data = snap.after.data();
     data.objectID = context.params.articleId;
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.saveObject(data);
+    return articlesIndex.saveObject(data);
   });
 
 export const onArticleDeleted = functions.firestore
   .document("articles/{articleId}")
   .onDelete((snap, context) => {
     const objectID = context.params.articleId;
-    const index = client.initIndex(ALGOLIA_INDEX_NAME);
-    return index.deleteObject(objectID);
+    return articlesIndex.deleteObject(objectID);
   });
