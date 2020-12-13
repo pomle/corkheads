d19212e0e28d760d49c21dc854df29402ec0e47b
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-13 10:53:13 +0100

    Use useStableIds in store

--- app/src/components/hooks/store2/useStore.ts
+++ app/src/components/hooks/store2/useStore.ts
@@ -2,10 +2,10 @@ import { firestore } from "firebase/app";
 import { Entry } from "types/Entry";
 import { useStableIndex } from "./useStableIndex";
 import { useSubscribers } from "./useSubscribers";
-import { useEqualList } from "./useEqualList";
 import { useIndex } from "./useIndex";
 import { usePath } from "./usePath";
 import { ResultMap } from "./ResultMap";
+import { useStableIds } from "./useStableIds";
 
 export function useFlatResult<T>(id: string, result: ResultMap<T>) {
   return result ? result.get(id) : null;
@@ -15,7 +15,7 @@ export function useStore<T>(
   collection: firestore.CollectionReference<T>,
   unstableIds: string[]
 ): ResultMap<Entry<T>> {
-  const ids = useEqualList(unstableIds);
+  const ids = useStableIds(unstableIds);
 
   const path = usePath(collection);
 
