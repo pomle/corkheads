1485add5931d4abde81e853797a223e2da8aad75
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-13 08:41:50 +0100

    Format BottlingMeta better

--- app/src/components/fragments/Article/SearchArticleItem/components/BottlingMeta/BottlingMeta.tsx
+++ app/src/components/fragments/Article/SearchArticleItem/components/BottlingMeta/BottlingMeta.tsx
@@ -1,16 +1,29 @@
+import { asABV } from "lib/format/stringify";
 import React from "react";
 import { Bottling } from "types/Bottling";
 
-function interleave(parts: React.ReactElement[], sep: React.ReactElement) {
+function interleave(parts: React.ReactElement[], sep: React.ReactFragment) {
   const output: React.ReactElement[] = [];
   for (let i = 0; i < parts.length; i += 1) {
     output.push(parts[i]);
     if (parts[i + 1]) {
-      output.push(sep);
+      output.push(<React.Fragment key={`sep-${i}`}>{sep}</React.Fragment>);
     }
   }
   return output;
 }
+
+const Age: React.FC<{ years: number }> = ({ years }) => {
+  return <>{years} years</>;
+};
+
+const ABV: React.FC<{ percent: number }> = ({ percent }) => {
+  return <>{asABV(percent)}</>;
+};
+
+const Year: React.FC<{ year: number }> = ({ year }) => {
+  return <>{year}</>;
+};
 
 interface BottlingMetaProps {
   bottling: Partial<Bottling>;
@@ -19,19 +32,19 @@ interface BottlingMetaProps {
 const BottlingMeta: React.FC<BottlingMetaProps> = ({ bottling }) => {
   const parts: React.ReactElement[] = [];
   if (bottling?.year) {
-    parts.push(<>{bottling.year}</>);
+    parts.push(<Year key="year" year={bottling.year} />);
   }
 
   const distill = bottling.distill;
   if (distill?.age) {
-    parts.push(<>{distill.age} years</>);
+    parts.push(<Age key="distill-age" years={distill.age} />);
   }
 
   if (distill?.alcoholByVolumePercentage) {
-    parts.push(<>{distill.alcoholByVolumePercentage}%</>);
+    parts.push(<ABV key="abv" percent={distill.alcoholByVolumePercentage} />);
   }
 
-  return <>{interleave(parts, <> – </>)}</>;
+  return <>{interleave(parts, " – ")}</>;
 };
 
 export default BottlingMeta;
