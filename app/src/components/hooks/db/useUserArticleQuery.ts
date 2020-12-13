e3397d51164e29ff2439657ade65e65d46e34d5e
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-13 15:56:20 +0100

    Revert "Swap userId and articleIds"
    This reverts commit c260915c69731d75b54329e407ed41081f400f85.

--- app/src/components/hooks/db/useUserArticleQuery.ts
+++ app/src/components/hooks/db/useUserArticleQuery.ts
@@ -49,5 +49,5 @@ export function useUserArticleQuery(query: UserArticleQuery) {
   }, [db, query]);
 
   const userId = query.filters.userId;
-  return useUserArticleTuple(ids, userId);
+  return useUserArticleTuple(userId, ids);
 }
