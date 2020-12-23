508ab58b493b59ed2fd0c947968ddc62e8edef5f
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-23 21:14:10 +0100

    Remove wrong return type

--- app/src/components/hooks/useMe.ts
+++ app/src/components/hooks/useMe.ts
@@ -1,8 +1,7 @@
 import { useSession } from "components/context/SessionContext";
-import { User } from "types/User";
 import { useUser } from "./db/useUsers";
 
-export function useMe(): User | undefined {
+export function useMe() {
   const session = useSession();
   return useUser(session?.user?.uid);
 }
