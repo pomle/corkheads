bb848d7af4bb651661d3827b9045c7393256790a
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-24 10:21:39 +0100

    Tweak check in item

--- app/src/components/fragments/CheckIn/CheckInItem/CheckInItem.tsx
+++ app/src/components/fragments/CheckIn/CheckInItem/CheckInItem.tsx
@@ -4,29 +4,38 @@ import { Article } from "types/Article";
 import { CheckIn } from "types/CheckIn";
 import PassedTime from "components/ui/format/PassedTime";
 import ImageItem from "components/ui/layout/ImageItem";
-import { Theme } from "components/ui/theme/themes";
+import { Colors, Theme } from "components/ui/theme/themes";
 import ItemRating from "components/fragments/Rating/ItemRating";
+import { useUser } from "components/hooks/db/useUsers";
+import { User } from "types/User";
 
 const useStyles = makeStyles((theme: Theme) => ({
   displayName: {
     color: theme.color.action,
     fontSize: "14px",
     fontWeight: 700,
-    gridColumn: "1 / 3",
+    gridArea: "1 / 1 / 2 / 2",
   },
   meta: {
     color: theme.color.text,
     fontSize: "12px",
     fontWeight: 500,
   },
-  timestamp: {
-    color: theme.color.text + "60",
+  checkInMeta: {
+    alignItems: "center",
+    color: Colors.X2,
+    display: "flex",
     fontSize: "10px",
-    textAlign: "right",
+    gap: "8px",
+    gridArea: "3 / 1 / 4 / 2",
   },
-  rating: {
-    gridColumn: "1 / 3",
+  username: {
+    color: Colors.X1,
   },
+  timestamp: {
+    color: Colors.X1,
+  },
+  rating: {},
 }));
 
 function resolvePhotoURL(checkIn: CheckIn, article: Article) {
@@ -41,28 +50,49 @@ function resolvePhotoURL(checkIn: CheckIn, article: Article) {
   return;
 }
 
+function resolveUserDisplayName(user?: User) {
+  if (user) {
+    if (user.username) {
+      return `@${user.username}`;
+    }
+    if (user.profile?.displayName) {
+      return user.profile.displayName;
+    }
+  }
+  return "• • •";
+}
+
 interface CheckInItemProps {
   checkIn: CheckIn;
   article: Article;
 }
 
 const CheckInItem: React.FC<CheckInItemProps> = ({ checkIn, article }) => {
-  const photoURL = resolvePhotoURL(checkIn, article);
+  const user = useUser(checkIn.userId)?.data;
 
-  const { displayName } = article;
+  const userDisplayName = resolveUserDisplayName(user);
+
+  const { displayName: articleDisplayName } = article;
   const { rating, timestamp } = checkIn;
+
+  const photoURL = resolvePhotoURL(checkIn, article);
 
   const classes = useStyles();
 
   return (
     <ImageItem imageURL={photoURL}>
-      <div className={classes.displayName}>{displayName}</div>
+      <div className={classes.displayName}>{articleDisplayName}</div>
       <blockquote className={classes.meta}>{checkIn.comment}</blockquote>
-      <div className={classes.timestamp}>
-        {timestamp && <PassedTime date={timestamp} />}
-      </div>
-      <div className={classes.rating}>
-        <ItemRating rating={rating} />
+      <div className={classes.checkInMeta}>
+        <div className={classes.rating}>
+          <ItemRating rating={rating} />
+        </div>
+        {" – "}
+        <div className={classes.timestamp}>
+          {timestamp && <PassedTime date={timestamp} />}
+        </div>
+        {" – "}
+        <div className={classes.username}>{userDisplayName}</div>
       </div>
     </ImageItem>
   );
