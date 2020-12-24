5eb54a8fac175dea249484a77ed901c0d63cf5f2
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-24 17:12:43 +0100

    Tweak Find a drink button

--- app/src/components/views/FindDrinkOverlayView/FindDrinkOverlayView.tsx
+++ app/src/components/views/FindDrinkOverlayView/FindDrinkOverlayView.tsx
@@ -1,45 +1,34 @@
 import React from "react";
 import * as Trans from "./locales";
 import { makeStyles } from "@material-ui/styles";
-import { Colors, Theme } from "components/ui/theme/themes";
+import { Colors } from "components/ui/theme/themes";
 
-const useStyles = makeStyles((theme: Theme) => {
-  const color = Colors.Navy;
-
-  return {
-    FindDrinkOverlayView: {
-      height: "100%",
-      pointerEvents: "none",
-      position: "relative",
-      width: "100%",
-    },
-    button: {
-      alignItems: "center",
-      background: `radial-gradient(
-      circle closest-side,
-      ${color} 0,
-      ${color} 44px,
-      transparent 45px,
-      transparent 45.5px,
-      ${color} 47px,
-      ${color}
-    )`,
-      borderRadius: "50%",
-      bottom: "16px",
-      color: "#fff",
-      display: "flex",
-      fontSize: "16px",
-      fontWeight: 500,
-      justifyContent: "center",
-      height: "96px",
-      padding: "16px",
-      pointerEvents: "all",
-      position: "absolute",
-      right: "16px",
-      textAlign: "center",
-      width: "96px",
-    },
-  };
+const useStyles = makeStyles({
+  FindDrinkOverlayView: {
+    height: "100%",
+    pointerEvents: "none",
+    position: "relative",
+    width: "100%",
+  },
+  button: {
+    alignItems: "center",
+    background: Colors.Navy,
+    borderRadius: "50%",
+    bottom: "16px",
+    boxShadow: "0 8px 16px #00000050",
+    color: "#fff",
+    display: "flex",
+    fontSize: "16px",
+    fontWeight: 500,
+    justifyContent: "center",
+    height: "96px",
+    padding: "16px",
+    pointerEvents: "all",
+    position: "absolute",
+    right: "16px",
+    textAlign: "center",
+    width: "96px",
+  },
 });
 
 interface FindDrinkOverlayViewProps {
