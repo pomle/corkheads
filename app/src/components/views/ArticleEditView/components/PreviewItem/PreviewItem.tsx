46512b0f87dd9ff5b74367e4eb5a5f7fecd18a07
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-25 06:40:49 +0100

    Remove duplicate BottlingMeta component

--- app/src/components/views/ArticleEditView/components/PreviewItem/PreviewItem.tsx
+++ app/src/components/views/ArticleEditView/components/PreviewItem/PreviewItem.tsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/styles";
 import ImageItem from "components/ui/layout/ImageItem";
 import { Theme } from "components/ui/theme/themes";
 import ItemRating from "components/fragments/Rating/ItemRating";
-import BottlingMeta from "./components/BottlingMeta";
+import BottlingMeta from "components/fragments/Bottling/BottlingMeta";
 import { Article } from "types/Article";
 
 const useStyles = makeStyles((theme: Theme) => ({
