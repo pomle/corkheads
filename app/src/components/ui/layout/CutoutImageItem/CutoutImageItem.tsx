25e5046f86f0684b9a11b91acf0ad495aa4f79f1
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-25 22:16:50 +0100

    Add CutoutImageItem

---
+++ app/src/components/ui/layout/CutoutImageItem/CutoutImageItem.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
+import { Theme } from "components/ui/theme/themes";
+import Photo from "components/ui/layout/Photo";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  CutoutImageItem: {
+    alignItems: "center",
+    background: theme.color.surface,
+    borderRadius: "12px",
+    display: "grid",
+    gridAutoFlow: "column",
+    gridTemplateColumns: "64px auto",
+    minHeight: "64px",
+    overflow: "hidden",
+  },
+  photo: {
+    height: "100%",
+    width: "100%",
+  },
+  content: {
+    color: theme.color.text,
+  },
+}));
+
+interface CutoutImageItemProps {
+  photoURL?: string;
+}
+
+const CutoutImageItem: React.FC<CutoutImageItemProps> = ({
+  photoURL,
+  children,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.CutoutImageItem}>
+      <div className={classes.photo}>
+        <Photo url={photoURL} />
+      </div>
+      <div className={classes.content}>{children}</div>
+    </div>
+  );
+};
+
+export default CutoutImageItem;
