838b57a5bb30256a8cebdaf176f8cfdf2cc4873f
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-25 08:00:45 +0100

    Tweak Badge component

--- app/src/components/ui/icons/Badge/Badge.tsx
+++ app/src/components/ui/icons/Badge/Badge.tsx
@@ -1,33 +1,55 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import { ReactComponent as BadgeIcon } from "assets/graphics/icons/badge.svg";
-import { ReactComponent as DiamondIcon } from "assets/graphics/icons/diamond.svg";
-import { ReactComponent as HeartIcon } from "assets/graphics/icons/heart.svg";
+import BadgeIcon from "assets/graphics/icons/badge.svg";
+import DiamondIcon from "assets/graphics/icons/diamond.svg";
+import HeartIcon from "assets/graphics/icons/heart.svg";
+import { Colors } from "components/ui/theme/themes";
 
-type Icon = "badge" | "diamond" | "heart";
+type Type = "badge" | "diamond" | "heart";
 
-const ICONS: Record<Icon, React.ReactNode> = {
+const ICONS: Record<Type, React.ReactNode> = {
   badge: BadgeIcon,
   diamond: DiamondIcon,
   heart: HeartIcon,
 };
 
+const COLORS: Record<Type, string> = {
+  badge: Colors.X1,
+  diamond: Colors.ShinyGold,
+  heart: Colors.Hearty,
+};
+
+type StyleProps = {
+  type: Type;
+};
+
 const useStyles = makeStyles({
-  Badge: {},
+  Badge: {
+    alignItems: "center",
+    backgroundImage: (props: StyleProps) => `url(${ICONS[props.type]})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "contain",
+    color: (props: StyleProps) => COLORS[props.type],
+    display: "flex",
+    fontFamily: "Roboto",
+    fontSize: "14px",
+    fontWeight: 700,
+    justifyContent: "center",
+    height: "2em",
+    position: "relative",
+    textAlign: "center",
+    width: "2em",
+  },
 });
 
 interface BadgeProps {
-  icon: Icon;
+  type?: Type;
 }
 
-const Badge: React.FC<BadgeProps> = ({ children, icon }) => {
-  const classes = useStyles();
-  return (
-    <div className={classes.Badge}>
-      {ICONS[icon]}
-      {children}
-    </div>
-  );
+const Badge: React.FC<BadgeProps> = ({ children, type = "badge" }) => {
+  const classes = useStyles({ type });
+  return <div className={classes.Badge}>{children}</div>;
 };
 
 export default Badge;
