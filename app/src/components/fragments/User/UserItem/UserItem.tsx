4169747410b1f657dc02a7c877d4ace68a0e594d
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-25 21:48:43 +0100

    Refactor UserItem

--- app/src/components/fragments/User/UserItem/UserItem.tsx
+++ app/src/components/fragments/User/UserItem/UserItem.tsx
@@ -1,47 +1,58 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import ImageItem from "components/ui/layout/ImageItem";
 import { Theme } from "components/ui/theme/themes";
-import { User } from "types/User";
+import { Colors } from "components/ui/theme/colors";
+import Photo from "components/ui/layout/Photo";
+import { useUser } from "components/hooks/db/useUsers";
 
 const useStyles = makeStyles((theme: Theme) => ({
+  UserItem: {
+    alignItems: "center",
+    display: "grid",
+    gridAutoFlow: "column",
+    gridTemplateColumns: "72px auto",
+  },
+  photo: {
+    borderRadius: "50%",
+    overflow: "hidden",
+    height: "64px",
+    width: "64px",
+  },
+  content: {
+    display: "grid",
+    gridGap: "4px",
+    padding: "8px",
+  },
   displayName: {
     color: theme.color.action,
     fontSize: "14px",
     fontWeight: 700,
     gridColumn: "1 / 3",
   },
-  subText: {
-    color: theme.color.text,
-    fontSize: "12px",
-    fontWeight: 500,
-  },
-  timestamp: {
-    color: theme.color.text + "60",
-    fontSize: "10px",
-    textAlign: "right",
-  },
-  rating: {
-    gridColumn: "1 / 3",
-  },
 }));
 
 interface UserItemProps {
-  user: User;
+  pointer: { userId: string };
 }
 
-const UserItem: React.FC<UserItemProps> = ({ user }) => {
-  const profile = user.profile;
+const UserItem: React.FC<UserItemProps> = ({ pointer: { userId } }) => {
+  const user = useUser(userId)?.data;
+  const profile = user?.profile;
   const photoURL = profile?.photoURL;
 
   const classes = useStyles();
 
   return (
-    <ImageItem imageURL={photoURL}>
-      <div className={classes.displayName}>
-        {user.username ? `@${user.username}` : "-"}
+    <div className={classes.UserItem}>
+      <div className={classes.photo}>
+        <Photo url={photoURL} />
       </div>
-    </ImageItem>
+      <div className={classes.content}>
+        <div className="username">
+          {user?.username ? "@" + user.username : ""}
+        </div>
+      </div>
+    </div>
   );
 };
 
