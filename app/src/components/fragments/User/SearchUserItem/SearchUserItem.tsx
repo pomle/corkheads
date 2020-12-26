342295c300d6a5283a81d3647e032824f99285f1
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-26 10:05:51 +0100

    Compose UserSearchItem from UserItem

--- app/src/components/fragments/User/SearchUserItem/SearchUserItem.tsx
+++ app/src/components/fragments/User/SearchUserItem/SearchUserItem.tsx
@@ -1,47 +1,35 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
-import { Theme } from "components/ui/theme/themes";
-import RoundedImageItem from "components/ui/layout/RoundedImageItem";
-import { useUser } from "components/hooks/db/useUsers";
 import { SearchResult } from "components/hooks/db/useUserSearch";
+import UserItem from "../UserItem";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  content: {
-    display: "grid",
-    gridGap: "4px",
-    padding: "8px",
+const useStyles = makeStyles({
+  SearchUserItem: {
+    alignItems: "center",
+    display: "flex",
+    "& > *:first-child": {
+      flex: "1",
+    },
   },
-  displayName: {
-    color: theme.color.action,
-    fontSize: "14px",
-    fontWeight: 700,
-    gridColumn: "1 / 3",
+  state: {
+    flex: "0",
   },
-  state: {},
-}));
+});
 
 interface SearchUserItemProps {
   result: SearchResult;
 }
 
-const SearchUserItem: React.FC<SearchUserItemProps> = ({
-  result: { userId },
-}) => {
-  const user = useUser(userId)?.data;
-  const profile = user?.profile;
-  const photoURL = profile?.photoURL;
-
+const SearchUserItem: React.FC<SearchUserItemProps> = ({ result }) => {
   const classes = useStyles();
 
   return (
-    <RoundedImageItem photoURL={photoURL}>
-      <div className={classes.content}>
-        <div className={classes.displayName}>
-          {user?.username ? "@" + user.username : ""}
-        </div>
-        <div className={classes.state}>Friends?</div>
+    <div className={classes.SearchUserItem}>
+      <UserItem pointer={result} />
+      <div className={classes.state}>
+        <button type="button">Friends</button>
       </div>
-    </RoundedImageItem>
+    </div>
   );
 };
 
