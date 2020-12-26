112925c9a62f47c94b8e1e954853964424be3167
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-26 10:07:50 +0100

    Add UserSearchRoutes

---
+++ app/src/components/route/routes/SearchUserRoutes/SearchUserRoutes.tsx
@@ -0,0 +1,73 @@
+import React, { useMemo } from "react";
+import { useHistory } from "react-router-dom";
+import Screen from "components/route/Screen";
+import ViewStack from "components/ui/layout/ViewStack";
+import FindPage from "./pages/FindPage";
+import { SlideRight } from "components/ui/transitions/Slide";
+import { Path } from "lib/path";
+import { stringCodec } from "components/route/codecs";
+import UserRoutes from "components/route/routes/UserRoutes/UserRoutes";
+
+interface SearchUserRoutesProps {
+  origin: Path<{}>;
+  path: Path<{}>;
+}
+
+const SearchUserRoutes: React.FC<SearchUserRoutesProps> = ({
+  origin,
+  path,
+}) => {
+  const history = useHistory();
+
+  const paths = useMemo(
+    () => ({
+      user: path.append("/user/:userId", {
+        userId: stringCodec,
+      }),
+    }),
+    [path]
+  );
+
+  const routes = useMemo(
+    () => ({
+      parent() {
+        const url = origin.url({});
+        history.push(url);
+      },
+      here() {
+        const url = path.url({});
+        history.push(url);
+      },
+      user(userId: string) {
+        const url = paths.user.url({ userId });
+        history.replace(url);
+      },
+    }),
+    [path, paths, origin, history]
+  );
+
+  const findPageRoutes = useMemo(
+    () => ({
+      cancel: routes.parent,
+      user: routes.user,
+    }),
+    [routes]
+  );
+
+  return (
+    <ViewStack>
+      <FindPage routes={findPageRoutes} />
+      <Screen path={paths.user} transition={SlideRight}>
+        {(match) => (
+          <UserRoutes
+            origin={path}
+            path={match.path}
+            userId={match.params.userId}
+          />
+        )}
+      </Screen>
+    </ViewStack>
+  );
+};
+
+export default SearchUserRoutes;
