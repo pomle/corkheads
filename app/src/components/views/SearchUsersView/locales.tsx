e24d3dec12e6624e9a1473a5612b7d8cea290e63
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-26 10:07:24 +0100

    UserSearch

--- app/src/components/views/SearchUsersView/locales.tsx
+++ app/src/components/views/SearchUsersView/locales.tsx
@@ -1,6 +1,6 @@
 import { localize, Locale as LC } from "lib/i18n/localization";
 
-export const FindDrink = localize({
-  [LC.enGB]: "Find a drink",
-  [LC.svSE]: "Hitta dryck",
+export const FindFriend = localize({
+  [LC.enGB]: "Find freinds",
+  [LC.svSE]: "Hitta vänner",
 });
