a12fdd1db0a0a879552f47431e6ee1216a051473
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-28 13:25:02 +0100

    Change useLiveTime to always origin from actual now

--- app/src/components/hooks/useLiveTime.ts
+++ app/src/components/hooks/useLiveTime.ts
@@ -7,9 +7,17 @@ export const useLiveTime = (unit: Unit): Moment => {
   const [time, setTime] = useState<Moment>(initial);
 
   useEffect(() => {
-    const nextTime = time.clone().startOf(unit).add(1, unit);
-    const delay = nextTime.diff(time, "milliseconds");
-    const timer = setTimeout(setTime, delay, nextTime);
+    const delay = time
+      .clone()
+      .startOf(unit)
+      .add(1, unit)
+      .diff(time, "milliseconds");
+
+    const timer = setTimeout(() => {
+      const nextTime = moment().startOf(unit).add(1, unit);
+      setTime(nextTime);
+    }, delay);
+
     return () => clearTimeout(timer);
   }, [unit, time, setTime]);
 
