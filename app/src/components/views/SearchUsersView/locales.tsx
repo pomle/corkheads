8d497db06d22e48ae36df9d90d8033089504811d
Pontus Alexander <pontus.alexander@gmail.com>
2020-12-29 10:35:28 +0100

    Remove unused Search components

--- app/src/components/views/SearchUsersView/locales.tsx
+++
@@ -1,6 +0,0 @@
-import { localize, Locale as LC } from "lib/i18n/localization";
-
-export const FindFriend = localize({
-  [LC.enGB]: "Find freinds",
-  [LC.svSE]: "Hitta vänner",
-});
--- app/src/components/views/SearchView/locales.tsx
+++
@@ -1,6 +0,0 @@
-import { localize, Locale as LC } from "lib/i18n/localization";
-
-export const FindDrink = localize({
-  [LC.enGB]: "Find a drink",
-  [LC.svSE]: "Hitta dryck",
-});
