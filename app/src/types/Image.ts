43799f3330b2a1affaa0a9be0a560cc4860f9043
Pontus Alexander <pontus.alexander@gmail.com>
2021-01-04 20:22:25 +0100

    Add Image type

---
+++ app/src/types/Image.ts
@@ -0,0 +1,36 @@
+import { createConverter } from "lib/firestore/converter";
+
+type Size = {
+  x: number;
+  y: number;
+};
+
+type Format = {
+  url: string;
+  resolution: Size;
+};
+
+export type Image = {
+  id: string;
+  userId: string;
+  formats: Format[];
+};
+
+const DEFAULTS = {
+  id: "",
+  userId: "",
+  formats: [],
+};
+
+export const converter = createConverter<Image>({
+  to(image) {
+    return image;
+  },
+
+  from(snapshot) {
+    return {
+      ...DEFAULTS,
+      ...snapshot.data(),
+    };
+  },
+});
