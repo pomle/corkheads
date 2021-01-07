ac82d71d4bbe90af7227360386fd350d3ef4e88c
Pontus Alexander <pontus.alexander@gmail.com>
2021-01-07 21:44:18 +0100

    Prefer username over article name for check ins in article context

--- app/src/components/fragments/CheckIn/CheckInList/CheckInList.tsx
+++ app/src/components/fragments/CheckIn/CheckInList/CheckInList.tsx
@@ -1,28 +1,52 @@
 import React from "react";
 import ItemList from "components/ui/layout/ItemList";
 import CheckInItemButton from "components/fragments/CheckIn/CheckInItem/Button";
+import ArticleContextCheckInItemButton from "components/fragments/CheckIn/ArticleContextCheckInItem/Button";
+
+type CheckInContext = "general" | "article";
 
 interface CheckInListProps {
   pointers: { articleId: string; userId: string; checkInId: string }[];
   routes: {
     checkIn: (checkInId: string) => void;
   };
+  context?: CheckInContext;
 }
 
-const CheckInList: React.FC<CheckInListProps> = ({ pointers, routes }) => {
-  return (
-    <ItemList divided>
-      {pointers.map((pointer) => {
-        return (
-          <CheckInItemButton
-            key={pointer.checkInId}
-            pointer={pointer}
-            route={routes.checkIn}
-          />
-        );
-      })}
-    </ItemList>
-  );
+const CheckInList: React.FC<CheckInListProps> = ({
+  pointers,
+  routes,
+  context = "general",
+}) => {
+  if (context === "article") {
+    return (
+      <ItemList divided>
+        {pointers.map((pointer) => {
+          return (
+            <ArticleContextCheckInItemButton
+              key={pointer.checkInId}
+              pointer={pointer}
+              route={routes.checkIn}
+            />
+          );
+        })}
+      </ItemList>
+    );
+  } else {
+    return (
+      <ItemList divided>
+        {pointers.map((pointer) => {
+          return (
+            <CheckInItemButton
+              key={pointer.checkInId}
+              pointer={pointer}
+              route={routes.checkIn}
+            />
+          );
+        })}
+      </ItemList>
+    );
+  }
 };
 
 export default CheckInList;
