76c3b32a683d53c64554ba3546c3e214487c4aec
Pontus Alexander <pontus.alexander@gmail.com>
2021-01-09 13:51:05 +0100

    Article create date

--- app/src/types/Article.ts
+++ app/src/types/Article.ts
@@ -1,7 +1,10 @@
+import * as firebase from "firebase/app";
+import { Moment } from "moment";
 import { createConverter } from "lib/firestore/converter";
 import { RatingAggregate } from "./RatingAggregate";
 import { Bottling } from "./Bottling";
 import { upgrade } from "./versioning/Article/upgrade";
+import { toMoment } from "./convert";
 
 export type Article = {
   id: string;
@@ -11,6 +14,7 @@ export type Article = {
   imageId?: string;
   ratingAggregate?: RatingAggregate;
   userId?: string;
+  timestamp?: Moment;
 };
 
 export function createArticle(id: string): Article {
@@ -23,13 +27,18 @@ export const DEFAULTS = createArticle("corkheads-article");
 
 export const converter = createConverter<Article>({
   to(article) {
-    return article;
+    return {
+      ...article,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    };
   },
 
   from(snapshot) {
+    const data = snapshot.data();
     return upgrade({
       ...DEFAULTS,
       ...snapshot.data({ serverTimestamps: "estimate" }),
+      timestamp: data.timestamp ? toMoment(data.timestamp) : undefined,
     });
   },
 });
