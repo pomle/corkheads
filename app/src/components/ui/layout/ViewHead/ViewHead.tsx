f388e326deebfc25c9588d2611a74aedb64e228d
Pontus Alexander <pontus.alexander@gmail.com>
2021-01-25 22:07:52 +0100

    Tweak padding in ViewHead
    Prevents from eating into nav

--- app/src/components/ui/layout/ViewHead/ViewHead.tsx
+++ app/src/components/ui/layout/ViewHead/ViewHead.tsx
@@ -4,7 +4,7 @@ import { Theme } from "components/ui/theme/themes";
 
 const useStyles = makeStyles((theme: Theme) => ({
   ViewHead: {
-    padding: "16px",
+    padding: "16px 40px",
     "& h1": {
       color: theme.color.title,
       fontSize: "20px",
