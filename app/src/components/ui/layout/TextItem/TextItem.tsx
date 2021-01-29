123ea34c05db34e98464ea1edec33488179ecbca
Pontus Alexander <pontus.alexander@gmail.com>
2021-01-29 22:06:16 +0100

    Tweak TextItem

--- app/src/components/ui/layout/TextItem/TextItem.tsx
+++ app/src/components/ui/layout/TextItem/TextItem.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { makeStyles } from "@material-ui/styles";
+import { Theme } from "components/ui/theme/themes";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
   TextItem: {
-    fontSize: "12px",
+    fontSize: "14px",
     padding: "24px 8px",
     textAlign: "center",
+    "& button": {
+      color: theme.color.action,
+      display: "inline",
+      fontSize: "inherit",
+    },
   },
-});
+}));
 
 interface TextItemProps {}
 
