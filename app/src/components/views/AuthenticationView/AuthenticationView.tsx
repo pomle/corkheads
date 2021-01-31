58d71123e1cb008329e6804687788a539f45fe01
Pontus Alexander <pontus.alexander@gmail.com>
2021-01-31 21:43:03 +0100

    Move Login handling into LoginView

--- app/src/components/views/AuthenticationView/AuthenticationView.tsx
+++ app/src/components/views/AuthenticationView/AuthenticationView.tsx
@@ -1,33 +1,16 @@
-import React, { useCallback } from "react";
+import React from "react";
 import LoginView from "components/views/LoginView";
-import { useAsyncCallback } from "components/hooks/useAsyncCallback";
 import { useSession } from "components/context/SessionContext";
 import Lock from "components/ui/transitions/Lock";
 
 const AuthenticationView: React.FC = () => {
   const session = useSession();
 
-  const submitHandle = useAsyncCallback(
-    useCallback(
-      (credentials: { username: string; password: string }) => {
-        return session.auth
-          .signInWithEmailAndPassword(
-            credentials.username,
-            credentials.password
-          )
-          .catch((error) => {
-            console.error(error);
-          });
-      },
-      [session]
-    )
-  );
-
-  const shouldPromptUser = session.ready && !session.user && !submitHandle.busy;
+  const shouldPromptUser = session.ready && !session.user;
 
   return (
     <Lock active={shouldPromptUser}>
-      <LoginView onSubmit={submitHandle.callback} />
+      <LoginView />
     </Lock>
   );
 };
