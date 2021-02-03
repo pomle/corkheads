12192eda87c16445022011b8f28b7f1fb78b57c5
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-03 07:35:08 +0100

    Add onboarding flow

--- app/src/components/views/AuthenticationView/AuthenticationView.tsx
+++ app/src/components/views/AuthenticationView/AuthenticationView.tsx
@@ -1,16 +1,47 @@
-import React from "react";
-import LoginView from "components/views/LoginView";
+import React, { useMemo, useState } from "react";
+import LoginView from "components/views//LoginView";
+import OnboardView from "components/views/OnboardView";
 import { useSession } from "components/context/SessionContext";
 import Lock from "components/ui/transitions/Lock";
+import ViewStack from "components/ui/layout/ViewStack";
+import { SlideRight } from "components/ui/transitions/Slide";
+
+enum Section {
+  Onboard,
+  Login,
+  PasswordReset,
+}
 
 const AuthenticationView: React.FC = () => {
   const session = useSession();
+
+  const [section, setSection] = useState<Section>(Section.Onboard);
+
+  const routes = useMemo(() => {
+    return {
+      login: () => setSection(Section.Login),
+      signUp: () => setSection(Section.Onboard),
+      reset: () => setSection(Section.PasswordReset),
+    };
+  }, [setSection]);
 
   const shouldPromptUser = session.ready && !session.user;
 
   return (
     <Lock active={shouldPromptUser}>
-      <LoginView />
+      <ViewStack>
+        <OnboardView routes={routes} />
+        <SlideRight active={section === Section.Login}>
+          <ViewStack>
+            <LoginView routes={routes} />
+            <SlideRight active={section === Section.Login}>
+              <ViewStack>
+                <LoginView routes={routes} />
+              </ViewStack>
+            </SlideRight>
+          </ViewStack>
+        </SlideRight>
+      </ViewStack>
     </Lock>
   );
 };
