1120981cab820b6fc5c65aa4d37c57c825041ad0
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-10 10:16:03 +0100

    Track page views and users

--- app/src/components/route/routes/AppRoute/AppRoute.tsx
+++ app/src/components/route/routes/AppRoute/AppRoute.tsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import ViewStack from "components/ui/layout/ViewStack";
 import Slide, { SlideDirection } from "components/ui/transitions/Slide";
 import AuthenticationView from "components/views/AuthenticationView";
 import ContextMenuView from "components/views/ContextMenuView/ContextMenuView";
 import RootRoutes from "components/route/routes/RootRoutes";
+import { useFirebase } from "components/context/FirebaseContext";
 import { useMe } from "components/hooks/useMe";
+import config from "config/app.config.js";
 
 const AppRoute: React.FC = () => {
   const user = useMe();
+
+  const history = useHistory();
+  const { analytics } = useFirebase();
+
+  useEffect(() => {
+    analytics.setUserId(user ? user.id : "anonymous");
+  }, [analytics, user]);
+
+  useEffect(() => {
+    return history.listen((location) => {
+      analytics.logEvent("screen_view", {
+        screen_name: location.pathname,
+        app_name: "Corkheads",
+        app_env: config.environment,
+        app_version: config.version,
+      });
+    });
+  }, [analytics, history]);
 
   return (
     <ViewStack>
