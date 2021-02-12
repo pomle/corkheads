0a0b00d66579439b4bcd1e71ae4ed79f910b359b
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-12 15:16:41 +0100

    Add OKDialog

---
+++ app/src/components/ui/layout/OKDialog/OKDialog.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import makeStyles from "@material-ui/styles/makeStyles";
+import PopupDialog from "components/ui/layout/PopupDialog";
+import PopupDialogButtonSet from "components/ui/layout/PopupDialog/components/PopupDialogButtonSet";
+import ActionButton from "components/ui/trigger/ActionButton";
+
+const useStyles = makeStyles({
+  OKDialog: {
+    fontSize: "16px",
+    fontWeight: 500,
+    padding: "24px",
+    textAlign: "center",
+  },
+});
+
+interface OKDialogProps {
+  onConfirm: () => void;
+}
+
+const OKDialog: React.FC<OKDialogProps> = ({ onConfirm, children }) => {
+  const classes = useStyles();
+
+  return (
+    <PopupDialog>
+      <div className={classes.OKDialog}>{children}</div>
+      <PopupDialogButtonSet>
+        <ActionButton variant="primary" onClick={onConfirm}>
+          OK
+        </ActionButton>
+      </PopupDialogButtonSet>
+    </PopupDialog>
+  );
+};
+
+export default OKDialog;
