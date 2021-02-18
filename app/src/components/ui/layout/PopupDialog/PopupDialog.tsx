988f0706ed550cad10a8b30674cedf0523383e14
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-18 07:11:44 +0100

    Base popup dialog on theme

--- app/src/components/ui/layout/PopupDialog/PopupDialog.tsx
+++ app/src/components/ui/layout/PopupDialog/PopupDialog.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import makeStyles from "@material-ui/styles/makeStyles";
+import { Theme } from "components/ui/theme/themes";
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme: Theme) => ({
   PopupDialog: {
-    background: "#fff",
+    background: theme.color.surface,
     borderRadius: "8px",
     boxShadow: "0 8px 24px -4px #0006",
+    color: theme.color.text,
   },
-});
+}));
 
 const PopupDialog: React.FC = ({ children }) => {
   const classes = useStyles();
