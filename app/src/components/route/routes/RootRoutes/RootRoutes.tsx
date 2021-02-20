22c85097614539e589fa4f00ade2e12051d96004
Pontus Alexander <pontus.alexander@gmail.com>
2021-02-20 07:11:01 +0100

    Remove unused ViewStack

--- app/src/components/route/routes/RootRoutes/RootRoutes.tsx
+++ app/src/components/route/routes/RootRoutes/RootRoutes.tsx
@@ -1,14 +1,9 @@
 import React from "react";
-import ViewStack from "components/ui/layout/ViewStack";
 import HomeRoutes from "components/route/routes/HomeRoutes";
 import { paths } from "components/route/paths";
 
 const RootRoutes: React.FC = () => {
-  return (
-    <ViewStack>
-      <HomeRoutes path={paths.root} />
-    </ViewStack>
-  );
+  return <HomeRoutes path={paths.root} />;
 };
 
 export default RootRoutes;
