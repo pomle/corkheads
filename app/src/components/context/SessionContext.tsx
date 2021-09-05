ee4e53d37ff822bdff252c2524a76f93cd04b85d
Pontus Alexander <pontus.alexander@gmail.com>
2021-09-05 09:00:28 +0200

    Use FirebaseAuth context from package

--- app/src/components/context/SessionContext.tsx
+++ app/src/components/context/SessionContext.tsx
@@ -1,54 +1,13 @@
-import React, {
-  createContext,
-  useContext,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import React from "react";
 import { useFirebase } from "components/context/FirebaseContext";
-
-type SessionContextValue = {
-  ready: boolean;
-  auth: firebase.auth.Auth;
-  user?: firebase.User;
-};
-
-const Context = createContext<SessionContextValue | null>(null);
+import { FirebaseAuthContext, useFirebaseAuth } from "@pomle/react-firebase";
 
 export const SessionContext: React.FC = ({ children }) => {
   const { auth } = useFirebase();
 
-  const [ready, setReady] = useState<boolean>(false);
-  const [user, setUser] = useState<firebase.User>();
-
-  useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged((user) => {
-      if (user) {
-        setUser(user);
-      } else {
-        setUser(undefined);
-      }
-      setReady(true);
-    });
-    return unsubscribe;
-  }, [auth]);
-
-  const value = useMemo(
-    () => ({
-      ready,
-      auth,
-      user,
-    }),
-    [ready, auth, user]
-  );
-
-  return <Context.Provider value={value}>{children}</Context.Provider>;
+  return <FirebaseAuthContext auth={auth}>{children}</FirebaseAuthContext>;
 };
 
 export function useSession() {
-  const session = useContext(Context);
-  if (!session) {
-    throw new Error("useSession without SessionContext");
-  }
-  return session;
+  return useFirebaseAuth();
 }
