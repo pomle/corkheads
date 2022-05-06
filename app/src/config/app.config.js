ed1219c3188170844a0d72338880b314008e2643
Pontus Persson <pontus@cur8.co>
2022-05-06 15:31:54 +0200

    Remove inline env

--- app/src/config/app.config.js
+++ app/src/config/app.config.js
@@ -1,6 +1,6 @@
 const config = {
-  version: process.env.REACT_APP_VERSION,
-  environment: process.env.REACT_APP_ENVIRONMENT,
+  version: process.env.REACT_APP_VERSION ?? "-",
+  environment: process.env.REACT_APP_ENVIRONMENT ?? "-",
 };
 
 console.debug("App Config", config);
